5c4164c238ee9c5d57465b0373a5da91797fe6d4
timothyshen <timothyshen1@hotmail.com>
2024-02-05 10:32:18 +0000

    fix something

---
+++ app/api/answer/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  return NextResponse.redirect(
+    "https://github.com/horsefacts/echo-the-dolphin",
+    { status: 302 }
+  );
+}
--- app/api/chatFLock/route.ts
+++ app/api/chatFLock/route.ts
@@ -2,22 +2,22 @@ import { NextRequest, NextResponse } from "next/server";
 import { FlockFrameType } from "../../types";
 
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL; // Ensure this variable is correctly set in your environment
-const postUrl = `${NEXT_PUBLIC_URL}/api/chatFlock`;
+const postUrl = `${NEXT_PUBLIC_URL}/api/code`;
 
 export async function POST(req: NextRequest): Promise<Response> {
-  const { untrustedData } = await req.json();
-  const { buttonIndex, inputText } = untrustedData;
+  // const { untrustedData } = await req.json();
+  // const { buttonIndex, inputText } = untrustedData;
 
-  // Log the extracted values for debugging purposes
-  console.log(`Button Index: ${buttonIndex}, Input Text: ${inputText}`);
+  // // Log the extracted values for debugging purposes
+  // console.log(`Button Index: ${buttonIndex}, Input Text: ${inputText}`);
 
-  const ecosystem = FlockFrameType[buttonIndex];
-  console.log(ecosystem); // Placeholder - replace with actual logic to determine selected ecosystem
+  // const ecosystem = FlockFrameType[buttonIndex];
+  // console.log(ecosystem); // Placeholder - replace with actual logic to determine selected ecosystem
 
-  // Assuming NEXT_PUBLIC_URL is your environment variable for the base URL
+  // // Assuming NEXT_PUBLIC_URL is your environment variable for the base URL
   const imageUrl = `${NEXT_PUBLIC_URL}/assets/FLockLogo.svg`;
 
-  console.log("Ecosystem: ", ecosystem);
+  // console.log("Ecosystem: ", ecosystem);
   return new NextResponse(
     `<!DOCTYPE html>
       <html>
@@ -26,9 +26,10 @@ export async function POST(req: NextRequest): Promise<Response> {
           <meta property="og:title" content="Let FLock it up" />
           <meta property="og:image" content="${imageUrl}" />
           <meta property="fc:frame" content="vNext" />
-          <meta property="fc:frame:post_url" content="${postUrl}" />
           <meta property="fc:frame:image" content="${imageUrl}" />
-          <meta property="fc:frame:button:1" content="See code" />
+          <meta property="fc:frame:post_url" content="${postUrl}" />
+          <meta name="fc:frame:button:1" content="See code" />
+          <meta name="fc:frame:button:1:action" content="post_redirect" />
         </head>
         <body/>
       </html>`,
