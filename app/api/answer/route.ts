9f857cd4f7cbff529fb180dd1de929060a63e8b7
timothyshen <timothyshen1@hotmail.com>
2024-02-05 23:09:55 +0000

    add new fix

--- app/api/answer/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  return NextResponse.redirect(
-    "https://github.com/horsefacts/echo-the-dolphin",
-    { status: 302 }
-  );
-}
--- app/api/chatFLock/route.ts
+++ app/api/chatFLock/route.ts
@@ -1,52 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 
 const NEXT_PUBLIC_URL = process.env.NEXT_PUBLIC_URL; // Ensure this variable is correctly set in your environment
-const postUrl = `${NEXT_PUBLIC_URL}/api/answer`;
+const postUrl = `${NEXT_PUBLIC_URL}/api/select`;
 
 export async function POST(req: NextRequest) {
-  const body: { unstrustedData?: { buttonIndex: number } } = await req.json();
-
-  let buttontext = "";
-
-  if (!body.unstrustedData) {
-    return new NextResponse(JSON.stringify({ error: "Invalid request" }), {
-      status: 400,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  const body: { untrustedData: { buttonIndex: number } } = await req.json();
+  let buttonText = "What is Solana?";
+  if (body.untrustedData.buttonIndex === 1) {
+    buttonText = "What is Ethereum?";
   }
 
-  switch (body.unstrustedData.buttonIndex) {
-    case 1:
-      buttontext = "What is Ethereum?";
-      break;
-    case 2:
-      buttontext = "What is Solana?";
-      break;
-    case 3:
-      buttontext = "What is Scroll?";
-      break;
-    case 4:
-      buttontext = "What is Sui?";
-      break;
-    default:
-      buttontext = "Unknown";
-  }
   return new NextResponse(
     `<!DOCTYPE html>
         <html>
           <head>
-              <title>Let FLock it up</title>
+            <title>Let FLock it up</title>
             <meta property="og:title" content="Let FLock it up" />
             <meta property="og:image" content="${NEXT_PUBLIC_URL}/assets/FLockLogo.svg" />
             <meta property="fc:frame" content="vNext" />
             <meta property="fc:frame:image" content="${NEXT_PUBLIC_URL}/assets/FLockLogo.svg" />
-            <meta property="fc:frame:button:1" content="${buttontext}" />
+            <meta property="fc:frame:button:1" content="what is solana" />
             <meta property="fc:frame:post_url" content="${postUrl}" />
-          </head>
-          <body/>
-        </html>`,
+            </head>
+            <body/>
+            </html>`,
     {
       status: 200,
       headers: {
@@ -55,5 +32,3 @@ export async function POST(req: NextRequest) {
     }
   );
 }
-
-export const GET = POST;
